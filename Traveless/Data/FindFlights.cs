79cd23bf3bdbeb538d7fa0f0a28e1a9a9c24a9d2
dreamersnat <chan.li.yyc@gmail.com>
2023-07-04 19:01:42 -0600

    Add project files.

---
+++ Traveless/Data/FindFlights.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Traveless.Data
+{
+    public class Flight
+    {
+        public string FlightNumber { get; set; }
+        public string Airline { get; set; }
+        public string OriginAirport { get; set; }
+        public string DestinationAirport { get; set; }
+        public string DayOfWeek { get; set; }
+        public string DepartureTime { get; set; }
+        public string Price { get; set; }
+
+    }
+}
---
+++ Traveless/Data/FindFlightsService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Traveless.Data
+{
+    public class FlightManager
+    {
+        public List<Flight> FindFlights(string originAirport, string destinationAirport, string dayOfWeek)
+        {
+            List<Flight> matchingFlights = new List<Flight>();
+
+            string filePath = Path.Combine(AppContext.BaseDirectory, @"resources\flights.csv");
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                string[] fields = line.Split(',');
+                string csvflightNumber = fields[0];
+                string csvairline = fields[1];
+                string csvOriginAirport = fields[2];
+                string csvDestinationAirport = fields[3];
+                string csvDayOfWeek = fields[4];
+                string csvdepartureTime = fields[5];
+                string csvPrice = fields[6];
+
+                if (csvOriginAirport == originAirport && csvDestinationAirport == destinationAirport && csvDayOfWeek == dayOfWeek)
+                {
+                    matchingFlights.Add(new Flight
+                    {
+                        FlightNumber = csvflightNumber,
+                        Airline = csvairline,
+                        OriginAirport = csvOriginAirport,
+                        DestinationAirport = csvDestinationAirport,
+                        DayOfWeek = csvDayOfWeek,
+                        DepartureTime = csvdepartureTime,
+                        Price = csvPrice
+                    });
+                }
+            }
+
+            return matchingFlights;
+        }
+    }
+}		
